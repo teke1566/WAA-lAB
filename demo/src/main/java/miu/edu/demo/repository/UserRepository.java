f298dfee7869272109112caa4c4849598c087eb1
teke1566 <teke156605@gmail.com>
2023-04-19 17:03:31 -0500

    user repository

--- demo/src/main/java/miu/edu/demo/repository/UserRepository.java
+++ demo/src/main/java/miu/edu/demo/repository/UserRepository.java
@@ -2,12 +2,15 @@ package miu.edu.demo.repository;
 
 import miu.edu.demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findByPostsSizeGreaterThan(int size);
+    @Query("SELECT u FROM User u WHERE SIZE(u.posts) > :n")
+    List<User> findByPostsSizeGreaterThan(@Param("n") int n);
 }
 
