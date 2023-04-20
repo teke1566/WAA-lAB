289a57b77e9da3d9b1f6fe516b1f2fe83bb20f53
teke1566 <teke156605@gmail.com>
2023-04-20 04:24:42 -0500

    edit post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,8 @@
 const Post = ({ post, onClick }) => {
   return (
     <div onClick={() => onClick(post)}>
-     
+      <h3>{post.title}</h3>
+      <p>Author: {post.author}</p>
     </div>
   );
 };
